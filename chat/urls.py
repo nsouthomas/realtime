202a9acead1a5a96ad56d50a42e289757940be33
Pedro Thomas <impedrothomas@outlook.com>
2023-12-05 11:39:41 -0300

    chat funcionando com apelido

--- chat/urls.py
+++ chat/urls.py
@@ -4,5 +4,5 @@ from .views import IndexView, SalaView
 
 urlpatterns = [
     path ('', IndexView.as_view(), name = 'index'),
-    path ('chat/<str:nome_sala>/',  SalaView.as_view(), name='sala'),
+    path('chat/<str:nome_sala>/<str:apelido>/', SalaView.as_view(), name='sala_com_apelido'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -11,8 +11,12 @@ class SalaView(TemplateView):
     template_name = 'sala.html'
 
     def get_context_data(self, **kwargs): 
-        context = super(SalaView, self). get_context_data(**kwargs)
+        context = super(SalaView, self).get_context_data(**kwargs)
         context['nome_sala_json'] = mark_safe(
             json.dumps(self.kwargs['nome_sala'])
+        )
+        # Adicionar o apelido ao contexto
+        context['apelido_json'] = mark_safe(
+            json.dumps(self.kwargs.get('apelido', 'Usuário'))  # Use um valor padrão para apelido, se necessário
         )
         return context
